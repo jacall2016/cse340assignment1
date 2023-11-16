0594c2b127d25575d624fd75a9543cb7f6b13d2a
James Call <jcall2016@byui.edu>
2023-11-16 14:08:10 -0700

    updated server.js, baseController.js and model

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -22,9 +23,12 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // Index route
+app.get("/", baseController.buildHome);
+/*
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
+*/
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
