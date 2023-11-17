6c15f3ef0b3bbabdaeec21c3b61a14e594541d77
James Call <jcall2016@byui.edu>
2023-11-16 21:40:33 -0700

    added error handling

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-
+const utilities = require("./utilities/index")
 
 /* ***********************
  * View Engine and Templates
@@ -25,7 +25,7 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // Index route
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome))
 /*
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
@@ -34,6 +34,26 @@ app.get("/", function(req, res){
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
